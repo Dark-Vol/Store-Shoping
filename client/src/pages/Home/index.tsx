97cc1aedd50b447babb84a130ab7aba3873015a1
Dark-Vol <overlord28985@gmail.com>
2025-03-07 19:16:38 +0200

    Добавить страницу ошибки с соответствующими стилями и компонентом, а также обновить главную страницу с карточкой продукта

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import ProductCard from "../../components/CardsItem";
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            Home Page
+            <ProductCard
+                image="/assets/react.svg"
+                title="Музыкальный инструмент"
+                price={3500}
+                rating={5}
+                onAddToCart={() => console.log("Добавлено в корзину")}
+                imageLink="/assets/Guitars.png"
+            />
+
+        </div>
+    );
+};
+
+export default Home;
