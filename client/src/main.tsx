94feac2d370df1f6ddc96665b45e48bc07f9a66a
Dark-Vol <overlord28985@gmail.com>
2025-03-13 00:28:39 +0200

    Удаление неиспользуемых файлов и обновление зависимостей

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouterSystem from './RouterSystem';
-import "./scss/reset.scss"
-import ClickSpark from './ClickSpark'
+import "./scss/main.scss";
 
 
 const root = ReactDOM.createRoot(
@@ -10,15 +9,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <ClickSpark
-    sparkColor='black'
-    sparkSize={10}
-    sparkRadius={15}
-    sparkCount={8}
-    duration={400}
-  >
     <React.StrictMode>
       <RouterSystem />
     </React.StrictMode>
-  </ClickSpark>
 );
