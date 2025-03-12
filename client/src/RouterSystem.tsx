76710b64c9d49d9ad5a270b2297db9a421e6cd20
Dark-Vol <overlord28985@gmail.com>
2025-03-12 22:55:58 +0200

    Add the Router System component to manage application routes

---
+++ client/src/RouterSystem.tsx
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import Header from "./components/Header";
+import Main from "./pages/Home"
+import ErrorPage from "./pages/Error"
+import Footer from "./components/Footer";
+
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Suspense fallback="">
+        <Routes>
+          <Route path="/" element={<Main />}/>
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App
