a26dc093157d8d86abbc6446e7a52fb711f423ed
Dark-Vol <overlord28985@gmail.com>
2025-03-12 22:55:10 +0200

    Replace App component with RouterSystem in main entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import App from './App';
+import RouterSystem from './RouterSystem';
 import "./scss/reset.scss"
 
 
@@ -10,9 +9,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <RouterSystem />
     </React.StrictMode>
-  </BrowserRouter>
 );
