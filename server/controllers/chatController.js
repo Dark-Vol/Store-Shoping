7f637ec21ed5c40bb99081e7573024413148e4d4
Dark-Vol <overlord28985@gmail.com>
2025-03-13 12:45:55 +0200

    Refactor chatController: add user creation, login, relogin, support ticket management, and message handling methods

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -3,18 +3,25 @@ const jwt = require("jsonwebtoken");
 const { User, Support, Message, Administrator} = require("../models/models");
 
 class ChatController {
+    /* Создание аккаунта пользователя */
     static async createUser(req, res) {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).json("Введите корректный email и пароль");
+        const { username, email, password } = req.body; 
+        if (!username || !email || !password) {
+            return res.status(400).json("Введите корректные данные");
         }
         try {
-            const existingUser = await User.findOne({ where: { email:email } });
+            const existingUser = await User.findOne({where: { email } });
             if (existingUser) {
                 return res.status(400).json("Пользователь с таким email уже существует");
             }
             const hashedPassword = await bcrypt.hash(password, 4);
-            const user = await User.create({ email:email, password: hashedPassword });
+            const user = await User.create(
+                { 
+                    username, 
+                    email,
+                    password: hashedPassword 
+                }
+            );
             const token = jwt.sign({ id: user.id }, "secret_key", { expiresIn: "24h" });
             return res.status(201).json({ token });
         } catch (error) {
@@ -22,8 +29,9 @@ class ChatController {
         }
     }
 
+    /* Автороизация пользователя */
     static async loginUser(req, res) {
-        const { email, password } = req.body;
+        const {email, password } = req.body; 
         if (!email || !password) {
             return res.status(400).json("Введите email и пароль");
         }
@@ -43,21 +51,7 @@ class ChatController {
         }
     }
 
-    static async createSupportTicket(req, res) {
-        const { title, body } = req.body;
-        if (!title || !body) {
-            return res.status(400).json("Название тикета и описание обязательны.");
-        }
-        console.log(1)
-        try {
-            const ticket = await Support.create({ title, body });
-            return res.status(201).json(ticket);
-        } catch (error) {
-            res.status(500).json({ error: "Ошибка сервера. Попробуйте позже." });
-        }
-        console.log(2)
-    }
-
+    /* Повторная авторизация пользователя */
     static async reloginUser(req, res) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
@@ -77,7 +71,59 @@ class ChatController {
         }
     }
 
-    /*Сохранение и получение всех сообщениий */
+    /* Созание запроса к администратору */
+    static async createSupportTicket(req, res) {
+        const { title, body } = req.body;
+        if (!title || !body) {
+            return res.status(400).json("Название тикета и описание обязательны.");
+        }
+        try {
+            const ticket = await Support.create({ title, body });
+            return res.status(201).json(ticket);
+        } catch (error) {
+            res.status(500).json({ error: "Ошибка сервера. Попробуйте позже." });
+        }
+    }
+
+    /* Закритие запроса к администратору */
+    static async closeSupportTicket(req , res){
+        const { id } = req.body;
+        if (!id) {
+            return res.status(400).json("Введите ИД тикета");
+        }
+        try {
+            const ticket = await Support.findByPk(id);
+            if (!ticket) {
+                return res.status(404).json("Тикет не найден");
+            }
+            ticket.statusClose = true;
+            await ticket.save();
+            return res.status(200).json("Тикет закрыт");
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: "Ошибка сервера. Попробуйте позже." });
+        }
+    }
+
+    /* Получение всех запросов для администратора */
+    static async getStateTicket(req, res) {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json("Введите ИД тикета");
+        }
+        try {
+            const ticket = await Support.findByPk(id);
+            if (!ticket) {
+                return res.status(404).json("Тикет не найден");
+            }
+            return res.status(200).json({ statusClose: ticket.statusClose });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: "Ошибка сервера. Попробуйте позже." });
+        }
+    }
+
+    /*Сохранение сообщениий в чате */
     static async saveMessage(req, res) {
         const { role, text, room } = req.body;
         const token = req.headers.authorization
@@ -100,6 +146,8 @@ class ChatController {
             return res.status(500).json({ error: "Ошибка сервера. Попробуйте позже." });
         }
     }
+
+    /* Получение всех сообщениий в чате */
     static async getMessages(req, res) {
         const { room } = req.params;
         if (!room) {
@@ -113,7 +161,6 @@ class ChatController {
             return res.status(500).json({ error: "Ошибка сервера. Попробуйте позже." });
         }
     }
-    
 }
 
 module.exports = ChatController;
