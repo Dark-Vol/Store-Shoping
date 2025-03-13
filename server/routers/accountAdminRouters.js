538833ab598efd0fbea2c085765777242499c8c4
Dark-Vol <overlord28985@gmail.com>
2025-03-13 12:47:50 +0200

    i

--- server/routers/accountAdminRouters.js
+++ server/routers/accountAdminRouters.js
@@ -7,4 +7,5 @@ router.post("/login", AdminController.loginAdmin);
 router.post("/relogin", AdminController.reloginAdmin);
 router.get("/ticket", AdminController.showSupportTicket);
 
+
 module.exports = router;
