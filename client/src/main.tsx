cea7e422bd190dd21c4bc18216179277c04bbb87
Dark-Vol <overlord28985@gmail.com>
2025-03-18 12:03:52 +0200

    Add BrowserRouter to main.tsx for routing support and improve error handling for root element

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import RouterSystem from './RouterSystem';
-import "./scss/main.scss";
+import "./scss/_reset.scss";
 
-
+const rootElem = document.getElementById("root");
+if (!rootElem) {
+  throw new Error("root element not found");
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
+  <BrowserRouter>
     <React.StrictMode>
       <RouterSystem />
     </React.StrictMode>
+  </BrowserRouter>
 );
