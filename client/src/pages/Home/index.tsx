e2ace373b2859fcac53fdab70bee0e0cddedc50b
Dark-Vol <overlord28985@gmail.com>
2025-03-18 12:02:26 +0200

    Refactor Home component: update layout, enhance styling, and comment out ProductCard instances

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,21 +1,98 @@
-import ProductCard from "../../components/CardsItem";
+// import ProductCard from "../../components/CardsItem";
 import React from "react";
-
+import style from "./Home.module.scss";
 
 const Home: React.FC = () => {
     return (
-        <div>
-            Home Page
-            <ProductCard
-                image="/assets/react.svg"
-                title="Музыкальный инструмент"
-                price={3500}
-                rating={5}
-                onAddToCart={() => console.log("Добавлено в корзину")}
-                imageLink="/assets/Guitars.png"
-            />
+        <div className={style.wrapper}>
+            <main className={style.home}>
+                Home Page
+            </main>
         </div>
     );
 };
 
 export default Home;
+
+{/*  
+    <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+        <ProductCard
+        image="/assets/react.svg"
+        title="Музыкальный инструмент"
+        price={3500}
+        rating={5}
+        onAddToCart={() => console.log("Добавлено в корзину")}
+        imageLink="/assets/Guitars.png"
+    />
+*/}
