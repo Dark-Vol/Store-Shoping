7d2e77b9634b6696017554d84d02e90380ed2e30
Dark-Vol <overlord28985@gmail.com>
2025-03-19 22:59:18 +0200

    Refactor Error component SVG markup for improved readability

--- client/src/pages/Error/index.tsx
+++ client/src/pages/Error/index.tsx
@@ -23,7 +23,10 @@ const Error: React.FC = () => {
                         xmlns="http://www.w3.org/2000/svg"
                     >
                         <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                        <g 
+                            id="SVGRepo_tracerCarrier" 
+                            strokeLinecap="round" 
+                            strokeLinejoin="round"></g>
                         <g id="SVGRepo_iconCarrier">
                             <path 
                                 d="M6 12H18M6 12L11 7M6 12L11 17" 
