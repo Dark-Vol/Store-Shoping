6b8d5acac82251542e7f8c25ad9c765b0a9f20c2
Dark-Vol <overlord28985@gmail.com>
2025-03-23 18:59:34 +0200

    Add BtnChat component for initiating chat functionality

---
+++ client/src/components/Buttons/BtnChat/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BtnChat: React.FC = () => {
+    return (
+        <button className='btn_open_chat'>
+            Chat
+        </button>
+    )
+}
+
+export default BtnChat
