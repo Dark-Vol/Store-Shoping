9305cfb80a6aa750de75d485d7a751d20422771a
Dark-Vol <overlord28985@gmail.com>
2025-03-23 19:20:48 +0200

    Refactoring code for models

--- server/models/models.js
+++ server/models/models.js
@@ -1,200 +1,48 @@
-// const { Sequelize, DataTypes } = require("sequelize");
-// const sequelize = require("../config/db");
+const {Sequelize,DataTypes} = require('sequelize');
+const sequelize = require('../config/db.js');
 
-// // Customer Model
-// const User = sequelize.define("User", {
-//     customer_name: { type: DataTypes.STRING(255), allowNull: false },
-//     email: { type: DataTypes.STRING(255), unique: true, allowNull: false },
-//     password: { type: DataTypes.STRING(255), allowNull: false },
-//     confirmation_code: { type: DataTypes.TEXT },
-//     confirmation_time: { type: DataTypes.DATE },
-// });
 
-// const Administrator = sequelize.define("Administrator", {
-//     admin_name: { type: DataTypes.STRING(255), allowNull: false },
-//     email: { type: DataTypes.STRING, unique: true, allowNull: false },
-//     password: { type: DataTypes.STRING, allowNull: false },
-// });
-
-// const Support = sequelize.define("Support", {
-//     title: { type: DataTypes.STRING(255), allowNull: false },
-//     body: {type: DataTypes.STRING(255), allowNull: false},
-//     statusClose: { type: DataTypes.BOOLEAN, defaultValue: false },
-// });
-
-// const Message = sequelize.define("Message", {
-//     text: { type: DataTypes.TEXT, allowNull: false },
-//     room: { type: DataTypes.INTEGER, allowNull: false },
-// });
-
-// // Country Model
-// const Country = sequelize.define("Country", {
-//     country_name: { type: DataTypes.STRING(255), allowNull: false },
-// });
-
-// // Manufacturer Model
-// const Manufacturer = sequelize.define("Manufacturer", {
-//     manufacture_name: { type: DataTypes.STRING(255), allowNull: false },
-//     contact_info: { type: DataTypes.STRING(255) },
-//     website: { type: DataTypes.STRING(255) },
-// });
-
-// // Category Model
-// const Category = sequelize.define("Category", {
-//     category_name: { type: DataTypes.STRING(255), allowNull: false },
-// });
-
-// // Instrument Model
-// const Instrument = sequelize.define("Instrument", {
-//     instrument_name: { type: DataTypes.STRING(255), allowNull: false },
-//     description: { type: DataTypes.TEXT },
-// });
-
-// // Item Model
-// const Item = sequelize.define("Item", {
-//     serial_number: { type: DataTypes.STRING(255), allowNull: false },
-//     year_of_production: { type: DataTypes.INTEGER },
-//     price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
-//     characteristics: { type: DataTypes.STRING(255) },
-// });
-
-// // Cart Model
-// const Cart = sequelize.define("Cart", {
-//     session_id: { type: DataTypes.STRING(100) },
-//     status: { type: DataTypes.SMALLINT },
-//     firstName: { type: DataTypes.STRING(50) },
-//     lastName: { type: DataTypes.STRING(50) },
-//     mobile: { type: DataTypes.STRING(20) },
-// });
-
-// // Cart Item Model
-// const CartItem = sequelize.define("CartItem", {
-//     price: { type: DataTypes.FLOAT, allowNull: false },
-//     discount: { type: DataTypes.FLOAT },
-//     active: { type: DataTypes.BOOLEAN, defaultValue: true },
-// });
-
-// // User Order Model
-// const UserOrder = sequelize.define("UserOrder", {
-//     delivery_address: { type: DataTypes.STRING(255) },
-//     total_price: { type: DataTypes.DECIMAL(10, 2) },
-//     discount: { type: DataTypes.FLOAT },
-//     final_price: { type: DataTypes.DECIMAL(10, 2) },
-//     active: { type: DataTypes.BOOLEAN, defaultValue: true },
-// });
-
-// // Order Status Model
-// const OrderStatus = sequelize.define("OrderStatus", {
-//     status_name: { type: DataTypes.STRING(255), allowNull: false },
-// });
-
-// // Transaction Model
-// const Transaction = sequelize.define("Transaction", {
-//     mode: { type: DataTypes.SMALLINT },
-//     status: { type: DataTypes.SMALLINT },
-// });
-
-// // Order Item Model
-// const OrderItem = sequelize.define("OrderItem", {
-//     price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
-//     discount: { type: DataTypes.FLOAT },
-// });
-
-// // Relationships
-// User.belongsTo(Country);
-// Country.hasMany(User);
-
-// User.hasMany(Message);
-// Message.belongsTo(User);
-
-// Administrator.hasMany(Message);
-// Message.belongsTo(Administrator);
-
-// Support.hasMany(Message);
-// Message.belongsTo(Support);
-
-// Item.belongsTo(Instrument);
-// Instrument.hasMany(Item);
-
-// Instrument.belongsTo(Category);
-// Category.hasMany(Instrument);
-
-// Item.belongsTo(Manufacturer);
-// Manufacturer.hasMany(Item);
-
-// Cart.belongsTo(User);
-// User.hasMany(Cart);
-
-// CartItem.belongsTo(Cart);
-// Cart.hasMany(CartItem);
-
-// UserOrder.belongsTo(User);
-// User.hasMany(UserOrder);
-
-// UserOrder.belongsTo(OrderStatus);
-// OrderStatus.hasMany(UserOrder);
-
-// Transaction.belongsTo(User);
-// User.hasMany(Transaction);
-
-// OrderItem.belongsTo(UserOrder);
-// UserOrder.hasMany(OrderItem);
-
-// OrderItem.belongsTo(Item);
-// Item.hasMany(OrderItem);
-
-// module.exports = {
-//     User,
-//     Country,
-//     Manufacturer,
-//     Category,
-//     Instrument,
-//     Item,
-//     Cart,
-//     CartItem,
-//     UserOrder,
-//     OrderStatus,
-//     Transaction,
-//     OrderItem,
-//     Administrator,
-//     Support,
-//     Message,
-// };
-
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("../config/db");
-
-// Customer Model
-const User = sequelize.define("User", {
-    customer_name: { type: DataTypes.STRING(255), allowNull: false },
-    email: { type: DataTypes.STRING(255), unique: true, allowNull: false },
-    password: { type: DataTypes.STRING(255), allowNull: false },
-    confirmation_code: { type: DataTypes.TEXT },
-    confirmation_time: { type: DataTypes.DATE },
-});
-
-const Administrator = sequelize.define("Administrator", {
-    admin_name: { type: DataTypes.STRING(255), allowNull: false },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    password: { type: DataTypes.STRING, allowNull: false },
+/** Создание акаунта, профиля для Администратора и Пользователя */
+const Account = sequelize.define("Account", {
+    accountType: { type: DataTypes.ENUM("User", "Administrator"), allowNull: false },
+    status: { type: DataTypes.STRING(50), allowNull: false, defaultValue: "active" },
 });
 
 const Profile = sequelize.define("Profile", {
-    first_name: { type: DataTypes.STRING(255), allowNull: false },
-    last_name: { type: DataTypes.STRING(255), allowNull: false },
+    firstName: { type: DataTypes.STRING(255), allowNull: false },
+    lastName: { type: DataTypes.STRING(255), allowNull: false },
     phone: { type: DataTypes.STRING(50) },
-    address: { type: DataTypes.STRING(255) },
+});
+
+const User = sequelize.define('User', {
+    userName: { type: DataTypes.STRING(255), allowNull: false },
+    email: { type: DataTypes.STRING(255), unique: true, allowNull: false },
+    password: { type: DataTypes.STRING(255), allowNull: false },
+    confirmationCode: { type: DataTypes.TEXT },
+    confirmationTime: { type: DataTypes.DATE },
+});
+
+const Administrator = sequelize.define("Administrator", {
+    adminName: { type: DataTypes.STRING(255), allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
 });
 
 const Photo = sequelize.define("Photo", {
     path: { type: DataTypes.STRING, allowNull: false, unique: true }
 });
 
-const Account = sequelize.define("Account", {
-    account_type: { type: DataTypes.ENUM("User", "Administrator"), allowNull: false },
-    status: { type: DataTypes.STRING(50), allowNull: false, defaultValue: "active" },
+const Address = sequelize.define("Address", {
+    address: { type: DataTypes.STRING(255), allowNull: false },
+    city: { type: DataTypes.STRING(255), allowNull: false },
+    zipCode: { type: DataTypes.STRING(50), allowNull: false },
+    region: { type: DataTypes.STRING(255) },
+    state: { type: DataTypes.STRING(255) },
+    other: { type: DataTypes.STRING(255) },
 });
+/** */
 
+/** Сообщения, Помощь */
 const Support = sequelize.define("Support", {
     title: { type: DataTypes.STRING(255), allowNull: false },
     body: {type: DataTypes.STRING(255), allowNull: false},
@@ -205,122 +53,75 @@ const Message = sequelize.define("Message", {
     text: { type: DataTypes.TEXT, allowNull: false },
     room: { type: DataTypes.INTEGER, allowNull: false },
 });
+/** */
 
-// Country Model
-const Country = sequelize.define("Country", {
-    country_name: { type: DataTypes.STRING(255), allowNull: false },
-});
-
-// Manufacturer Model
-const Manufacturer = sequelize.define("Manufacturer", {
-    manufacture_name: { type: DataTypes.STRING(255), allowNull: false },
-    contact_info: { type: DataTypes.STRING(255) },
-    website: { type: DataTypes.STRING(255) },
-});
-
-// Category Model
+/** Предметы */
 const Category = sequelize.define("Category", {
-    category_name: { type: DataTypes.STRING(255), allowNull: false },
+    categoryName: { type: DataTypes.STRING(255), allowNull: false },
 });
 
-// Instrument Model
 const Instrument = sequelize.define("Instrument", {
-    instrument_name: { type: DataTypes.STRING(255), allowNull: false },
+    instrumentName: { type: DataTypes.STRING(255), allowNull: false },
     description: { type: DataTypes.TEXT },
 });
 
-// Item Model
 const Item = sequelize.define("Item", {
-    serial_number: { type: DataTypes.STRING(255), allowNull: false },
-    year_of_production: { type: DataTypes.INTEGER },
+    serialNumber: { type: DataTypes.STRING(255), allowNull: false },
+    yearOfProduction: { type: DataTypes.INTEGER },
     price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
     characteristics: { type: DataTypes.STRING(255) },
 });
+/** */
 
-// Cart Model
+/** Оплата */
 const Cart = sequelize.define("Cart", {
-    session_id: { type: DataTypes.STRING(100) },
+    sessionId: { type: DataTypes.STRING(100) },
     status: { type: DataTypes.SMALLINT },
     firstName: { type: DataTypes.STRING(50) },
     lastName: { type: DataTypes.STRING(50) },
     mobile: { type: DataTypes.STRING(20) },
 });
 
-// Cart Item Model
 const CartItem = sequelize.define("CartItem", {
     price: { type: DataTypes.FLOAT, allowNull: false },
     discount: { type: DataTypes.FLOAT },
     active: { type: DataTypes.BOOLEAN, defaultValue: true },
 });
 
-// User Order Model
 const UserOrder = sequelize.define("UserOrder", {
-    delivery_address: { type: DataTypes.STRING(255) },
-    total_price: { type: DataTypes.DECIMAL(10, 2) },
+    deliveryAddress: { type: DataTypes.STRING(255) },
+    totalPrice: { type: DataTypes.DECIMAL(10, 2) },
     discount: { type: DataTypes.FLOAT },
-    final_price: { type: DataTypes.DECIMAL(10, 2) },
+    finalPrice: { type: DataTypes.DECIMAL(10, 2) },
     active: { type: DataTypes.BOOLEAN, defaultValue: true },
 });
 
-// Order Status Model
 const OrderStatus = sequelize.define("OrderStatus", {
-    status_name: { type: DataTypes.STRING(255), allowNull: false },
+    statusName: { type: DataTypes.STRING(255), allowNull: false },
 });
 
-// Transaction Model
 const Transaction = sequelize.define("Transaction", {
     mode: { type: DataTypes.SMALLINT },
     status: { type: DataTypes.SMALLINT },
 });
 
-// Order Item Model
 const OrderItem = sequelize.define("OrderItem", {
     price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
     discount: { type: DataTypes.FLOAT },
 });
+/** */
 
-// Relationships
-User.belongsTo(Country);
-Country.hasMany(User);
+/** Other */
+const Country = sequelize.define("Country", {
+    countryName: { type: DataTypes.STRING(255), allowNull: false },
+});
 
-User.hasMany(Message);
-Message.belongsTo(User);
-
-Administrator.hasMany(Message);
-Message.belongsTo(Administrator);
-
-Support.hasMany(Message);
-Message.belongsTo(Support);
-
-Item.belongsTo(Instrument);
-Instrument.hasMany(Item);
-
-Instrument.belongsTo(Category);
-Category.hasMany(Instrument);
-
-Item.belongsTo(Manufacturer);
-Manufacturer.hasMany(Item);
-
-Cart.belongsTo(User);
-User.hasMany(Cart);
-
-CartItem.belongsTo(Cart);
-Cart.hasMany(CartItem);
-
-UserOrder.belongsTo(User);
-User.hasMany(UserOrder);
-
-UserOrder.belongsTo(OrderStatus);
-OrderStatus.hasMany(UserOrder);
-
-Transaction.belongsTo(User);
-User.hasMany(Transaction);
-
-OrderItem.belongsTo(UserOrder);
-UserOrder.hasMany(OrderItem);
-
-OrderItem.belongsTo(Item);
-Item.hasMany(OrderItem);
+const Manufacturer = sequelize.define("Manufacturer", { // производитель
+    manufactureName: { type: DataTypes.STRING(255), allowNull: false },
+    contactInfo: { type: DataTypes.STRING(255) },
+    webSite: { type: DataTypes.STRING(255) },
+});
+/** */
 
 Profile.belongsTo(User);
 User.hasOne(Profile);
@@ -337,14 +138,95 @@ Administrator.hasOne(Account);
 Photo.belongsTo(Profile);
 Profile.hasOne(Photo);
 
+User.belongsTo(Country)
+Country.hasMany(User)
+
+Country.hasMany(Address) // так как одна страна может иметь несколько адресов
+Address.belongsTo(Country) // указывая, что каждый адрес принадлежит одной стране
+
+Country.hasMany(Manufacturer) // потому что одна страна может иметь несколько производителей
+Manufacturer.belongsTo(Country) // так как каждый производитель принадлежит одной стране
+
+Manufacturer.hasMany(Instrument) // потому что один производитель может выпускать несколько инструментов.
+Instrument.belongsTo(Manufacturer) // так как каждый инструмент принадлежит одному производителю.
+
+Category.hasMany(Instrument); // Каждый инструмент в одной категории
+Instrument.belongsTo(Category); // Инструмент может быть в нескольких категориях
+
+// Один инструмент может иметь несколько товаров
+Instrument.hasMany(Item);
+Item.belongsTo(Instrument);
+
+// Один товар может быть в нескольких записях корзины
+Item.hasMany(CartItem);
+CartItem.belongsTo(Item);
+
+// Один Country может иметь много Items
+Country.hasMany(Item);
+Item.belongsTo(Country);
+
+// Один User может иметь только одну Cart
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+// Одна Cart может содержать много CartItem
+Cart.hasMany(CartItem);
+CartItem.belongsTo(Cart);
+
+// Один Item может быть в нескольких CartItem
+Item.hasMany(CartItem);
+CartItem.belongsTo(Item);
+
+// Один User может иметь много UserOrder
+User.hasMany(UserOrder);
+UserOrder.belongsTo(User);
+
+// Один UserOrder имеет один OrderStatus
+OrderStatus.hasOne(UserOrder);
+UserOrder.belongsTo(OrderStatus);
+
+// Один UserOrder может содержать несколько OrderItem
+UserOrder.hasMany(OrderItem);
+OrderItem.belongsTo(UserOrder);
+
+// Один OrderItem может иметь одну Transaction
+OrderItem.hasOne(Transaction);
+Transaction.belongsTo(OrderItem);
+
+// Один User может иметь много Transaction
+User.hasMany(Transaction);
+Transaction.belongsTo(User);
+
+// Связь страны с адресом (Одна страна может содержать несколько адресов)
+Country.hasMany(Address);
+Address.belongsTo(Country);
+
+// Если адрес принадлежит пользователю
+Profile.hasOne(Address);
+Address.belongsTo(Profile);
+
+// Если адрес используется для доставки заказа
+UserOrder.belongsTo(Address);
+Address.hasOne(UserOrder);
+
+User.hasMany(Message);
+Message.belongsTo(User);
+
+Administrator.hasMany(Message);
+Message.belongsTo(Administrator);
+
+Support.hasMany(Message);
+Message.belongsTo(Support);
+
 module.exports = {
+    Account,
+    Profile,
+    Address,
+    Photo,
     User,
     Administrator,
-    Profile,
-    Photo,
-    Account,
     Country,
-    Manufacturer,
+    Manufacturer, // производитель
     Category,
     Instrument,
     Item,
