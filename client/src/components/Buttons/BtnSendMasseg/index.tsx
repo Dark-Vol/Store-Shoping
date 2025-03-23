5df4f30db7f5c2f594e4d70f1609f2a7498d2029
Dark-Vol <overlord28985@gmail.com>
2025-03-23 19:05:44 +0200

    Add BtnSendMasseg component for message sending functionality

---
+++ client/src/components/Buttons/BtnSendMasseg/index.tsx
@@ -0,0 +1,9 @@
+const BtnSendMasseg: React.FC = () => {
+    return (
+        <button className='btn_send_masseg'>
+            Закрыть тикет
+        </button>
+    )
+}
+
+export default BtnSendMasseg
