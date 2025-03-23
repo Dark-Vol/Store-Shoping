af17906f767b64ef57d1f283cad55c69d684edc5
Dark-Vol <overlord28985@gmail.com>
2025-03-23 19:00:41 +0200

    Add BtnCloseTicket component for closing tickets

---
+++ client/src/components/Buttons/BtnCloseTicket/index.tsx
@@ -0,0 +1,10 @@
+
+const btnCloseTicket: React.FC = () => {
+    return (
+        <button className='btn_close_ticket'>
+            Закрыть тикет
+        </button>
+    )
+}
+
+export default btnCloseTicket
