a2a537a6caff8a5ca22722d808a51b3af3be4253
Dark-Vol <overlord28985@gmail.com>
2025-03-23 19:06:43 +0200

    Add BtnStartChat component for ticket closure functionality

---
+++ client/src/components/Buttons/BtnStartChat/index.tsx
@@ -0,0 +1,9 @@
+const BtnStartChat: React.FC = () => {
+    return (
+        <button className='btn_start_chat'>
+            Закрыть тикет
+        </button>
+    )
+}
+
+export default BtnStartChat
