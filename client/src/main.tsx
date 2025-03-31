db3cfafb42cac3c8a058226a87a25f4b091a78f6
Dark-Vol <overlord28985@gmail.com>
2025-04-01 00:07:22 +0300

    Update main stylesheet import to use Main.scss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import RouterSystem from './RouterSystem';
-import "./scss/_reset.scss";
+import "./scss/Main.scss";
 
 const rootElem = document.getElementById("root");
 if (!rootElem) {
