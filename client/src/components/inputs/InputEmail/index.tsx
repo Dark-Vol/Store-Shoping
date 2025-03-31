4938dbe0e1ba5f3830a73c2902334b18fd7f032b
Dark-Vol <overlord28985@gmail.com>
2025-04-01 00:23:32 +0300

    Refactor InputEmail component to simplify state management and update styling

--- client/src/components/inputs/InputEmail/index.tsx
+++ client/src/components/inputs/InputEmail/index.tsx
@@ -1,37 +1,21 @@
-import { useState } from "react";
 import styles from "./InputEmail.module.scss";
 
-interface InputEmailProps {
-    onChange?: (value: string) => void;
-}
+const InputEmail: React.FC = () => {
 
-const InputEmail: React.FC<InputEmailProps> = ({onChange}) => {
-    const [text, setText] = useState("");
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setText(e.target.value);
-        if (onChange) {
-            onChange(e.target.value);
-        }
-    };
-    return (
-        <div className={styles.box}>
-            <input
-            id="email"
-                className={styles.input_email}
-                type="text"
-                placeholder="Email"
-                value={text}
-                onChange={handleChange}
-            />
-            <label 
-                htmlFor="email"
-                className={styles.label_email}
-            >
-                Email
-            </label>
-        </div>
-    )
+  return (
+    <div className={styles.coolinput}>
+      {/* <label htmlFor="email" className={styles.label}>
+        Email
+      </label> */}
+        <input
+          id="email"
+          className={`${styles.input}`}
+          type="email"
+          placeholder="Email"
+        />
+        <span>@gmail.com</span>
+    </div>
+  )
 }
 
 export default InputEmail
