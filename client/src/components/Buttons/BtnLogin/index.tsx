33a7ff3dfa4a34b76540111850503cb95b975861
Dark-Vol <overlord28985@gmail.com>
2025-04-01 00:13:15 +0300

    Refactor BtnLogin component to use CSS modules and update button structure

--- client/src/components/Buttons/BtnLogin/index.tsx
+++ client/src/components/Buttons/BtnLogin/index.tsx
@@ -1,10 +1,23 @@
+import styles from './BtnLogin.module.scss'
 
 const BtnLogin: React.FC = () => {
-    return (
-        <button className='btn_login'>
-            Закрыть тикет
-        </button>
-    )
+  return (
+
+    <button className={styles.btn_login}>
+      <svg viewBox="0 0 24 24" className={styles.arr2} xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+        ></path>
+      </svg>
+      <span className={styles.text}>SING IN </span>
+      <span className={styles.circle}></span>
+      <svg viewBox="0 0 24 24" className={styles.arr1} xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+        ></path>
+      </svg>
+    </button>
+  )
 }
 
 export default BtnLogin
