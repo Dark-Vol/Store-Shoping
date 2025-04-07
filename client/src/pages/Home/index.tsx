d449b969e9ce6a454a1340dec7350d67f8344377
Dark-Vol <overlord28985@gmail.com>
2025-04-07 23:16:31 +0300

    Refactor Home component to simplify structure and remove unused input fields

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import style from "./Home.module.scss";
-import InputEmail from "@components/Inputs/InputEmail";
-import InputPassword from "@components/Inputs/InputPassword";
-import InputPasswordConfirm from "@components/Inputs/InputPasswordConfirm";
 
 const Home: React.FC = () => {
-    return (
-        <div className={style.wrapper}>
-            <main className={style.home}>
-              Home Page
-              <InputEmail />
-              <InputPassword />
-              <InputPasswordConfirm  />
-            </main>
-        </div>
-    );
+  return (
+    <section className={style.wrapper}>
+      Home Page
+    </section>
+  );
 };
 
 export default Home;
