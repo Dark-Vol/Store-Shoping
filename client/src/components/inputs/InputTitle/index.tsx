7717580b0a8309e50c8c5a0e13a7cf0e69dbb62c
Dark-Vol <overlord28985@gmail.com>
2025-04-07 23:17:09 +0300

    Refactor InputTitle component to streamline props and improve input handling

--- client/src/components/inputs/InputTitle/index.tsx
+++ client/src/components/inputs/InputTitle/index.tsx
@@ -1,38 +1,21 @@
-import { useState } from "react";
 import styles from "./InputTitle.module.scss";
 
-interface InputTitleProps {
-    onChange?: (value: string) => void;
-}
+type InputTitleProps = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-const InputTitle: React.FC<InputTitleProps> = ({onChange}) => {
-    const [password, setPassword] = useState("");
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-        if (onChange) {
-            onChange(e.target.value);
-        }
-    };
-    
-    return (
-        <div className={styles.box}>
-        <input
-            id="title"
-            className={styles.input_title}
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handleChange}
-        />
-        <label 
-            htmlFor="title"
-            className={styles.label_title}
-        >
-            title
-        </label>
-        </div>
-    )
+const InputTitle: React.FC<InputTitleProps> = ({ value, onChange }) => {
+  return (
+    <div className={styles.box}>
+      <input
+        type="text"
+        className={styles.input_title}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
 }
 
 export default InputTitle
