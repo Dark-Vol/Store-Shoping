50c8219359b157ec60d9fce42724d92d3044df9a
Dark-Vol <overlord28985@gmail.com>
2025-05-27 14:29:42 +0300

    Remove ProductCard component from Products page for code simplification

--- client/src/pages/Products/index.tsx
+++ client/src/pages/Products/index.tsx
@@ -1,18 +1,7 @@
-import React from 'react'
-import ProductCard from '../../components/CardsItem'
 
 const Products: React.FC = () => {
   return (
     <section className=''>
-      <ProductCard
-        path="/product/1"
-        image="/assets/react.svg"
-        title="Музыкальный инструмент"
-        price={3500}
-        rating={5}
-        onAddToCart={() => console.log("Добавлено в корзину")}
-        imageLink="/assets/Guitars.png"
-      />
     </section>
   )
 }
