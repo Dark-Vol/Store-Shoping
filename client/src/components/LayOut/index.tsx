d60f8afa68b142151447b257ac60049a9a320f42
Dark-Vol <overlord28985@gmail.com>
2025-07-06 21:06:50 +0300

    Refactor LayOut component structure for improved readability and layout consistency

--- client/src/components/LayOut/index.tsx
+++ client/src/components/LayOut/index.tsx
@@ -2,15 +2,18 @@ import GetLayoutChat from '@components/Chat/GetLayoutChat';
 import Footer from '@components/Footer';
 import Header from '@components/Header';
 import { Outlet } from 'react-router-dom';
+import styles from './Layout.module.scss';
 
 const LayOut: React.FC = () => {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
-      <Outlet />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
       <GetLayoutChat />
       <Footer />
-    </>
+    </div>
   );
 };
 
