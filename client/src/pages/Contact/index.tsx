00f1360189d9e63657dfd3f1fd3942049ad13085
Dark-Vol <overlord28985@gmail.com>
2025-08-19 12:04:20 +0300

    refactor

--- client/src/pages/Contact/index.tsx
+++ client/src/pages/Contact/index.tsx
@@ -1,57 +1,3 @@
-// import { useState } from 'react';
-// import InputFirstName from '@components/Inputs/InputFirstName';
-// import InputPhoneNumber from '@components/Inputs/InputPhoneNumber';
-
-
-// import styles from './Contact.module.scss';
-
-
-// export default function ContactPage() {
-//   const [firstName, setFirstName] = useState("");
-//   const [phoneNumber, setPhoneNumber] = useState("");
-
-
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.text_center}>
-//         <h2 className={styles.text_title}>Свяжитесь с нами</h2>
-//         <p className={styles.text_description}>
-//           Есть вопросы о музыкальных инструментах?
-//           Нужна консультация?
-//           Мы всегда готовы помочь вам найти идеальный инструмент!
-//         </p>
-//       </div>
-//       <div className="">
-//         <div className="">
-//           <h1>Напишите нам</h1>
-//           <p>
-//             Заполните форму, и мы свяжемся с вами в ближайшее время
-//           </p>
-//           <form action="">
-//             <div className="">
-//               <div className="">
-//                 <label htmlFor="name">Имя *</label>
-//                 <InputFirstName
-//                   firstName={firstName}
-//                   onChange={(e) => setFirstName(e.target.value)}
-//                 />
-
-//               </div>
-//             </div>
-//             <div className="">
-//               <label htmlFor="phone">Телефон</label>
-//               <InputPhoneNumber
-//                 number={phoneNumber}
-//                 onChange={(e) => setPhoneNumber(e.target.value)}
-//               />
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { HeaderSection } from "@components/Сontacts/HeaderSection"
 import { ContactHero } from "@components/Сontacts/ContactHero"
 import { ContactInfoCard } from "@components/Сontacts/ContactInfoCard"
@@ -61,19 +7,36 @@ import { SocialMediaCard } from "@components/Сontacts/SocialMediaCard"
 import { ContactForm } from "@components/Сontacts/ContactForm"
 import { MapSection } from "@components/Сontacts/MapSection"
 import { ServicesSection } from "@components/Сontacts/ServicesSection"
+import styles from "./Contact.module.scss"
 
 export default function ContactPage() {
   return (
-    <div className="">
+    <div className={styles.contactPage}>
       <HeaderSection />
-      <ContactHero />
-      <ContactInfoCard />
-      <BusinessHoursCard />
-      <DepartmentContactsCard />
-      <SocialMediaCard />
-      <ContactForm />
-      <MapSection />
-      <ServicesSection />
+      <div className={styles.container}>
+        <div className={styles.heroSection}>
+          <ContactHero />
+        </div>
+        <div className={styles.contactCardsSection}>
+          <div className={styles.grid}>
+            <div className={styles.cardsColumn}>
+              <ContactInfoCard />
+              <BusinessHoursCard />
+              <DepartmentContactsCard />
+              <SocialMediaCard />
+            </div>
+            <div className={styles.formSection}>
+              <ContactForm />
+              <div className={styles.mapSection}>
+                <MapSection />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.servicesSection}>
+          <ServicesSection />
+        </div>
+      </div>
     </div>
   )
 }
