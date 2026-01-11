d209b1213c743da736eddfd6369b3476eda00ce3
Dark-Vol <overlord28985@gmail.com>
2026-01-11 16:01:18 +0200

    Delete server/routers/photoRoutes.js

--- server/routers/photoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const PhotoController = require("../controllers/photoController");
-
-const router = express.Router();
-
-router.get("/", PhotoController.getAllPhotos);
-router.get("/:id", PhotoController.getPhotoById);
-router.post("/", PhotoController.createPhoto);
-router.put("/:id", PhotoController.updatePhoto);
